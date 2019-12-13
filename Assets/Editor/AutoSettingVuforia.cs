cb8c35b26fd9149ee0888b082ea9608f5689909c
365462129@qq.com <365462129@qq.com>
2019-12-13 11:30:42 +0800

    no message

---
+++ Assets/Editor/AutoSettingVuforia.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Vuforia;
+
+public class AutoSettingVuforia
+{
+
+    [MenuItem("Tools/自动设置Vuforia")]
+    public static void DDD()
+    {
+        bool bbb1 =  EditorApplication.ExecuteMenuItem("GameObject/Vuforia/AR Camera");
+        bool bbb2 = EditorApplication.ExecuteMenuItem("GameObject/Vuforia/Image");
+
+        GameObject ImageTarget = GameObject.Find("ImageTarget");
+        ImageTarget.transform.SetParent(null);
+
+        VuforiaBehaviour vuforiaBehaviour = GameObject.Find("ARCamera").GetComponent<VuforiaBehaviour>();
+
+    }
+
+
+}
---
+++ Assets/Scripts/OnlyScanRecognition.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using Vuforia;
+
+/// <summary>
+///     A custom handler that implements the ITrackableEventHandler interface.
+/// </summary>
+public class OnlyScanRecognition : MonoBehaviour, ITrackableEventHandler
+{
+    #region PROTECTED_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+
+    #endregion // PROTECTED_MEMBER_VARIABLES
+    #region UNITY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PROTECTED_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        Debug.LogError("识别成功");
+    }
+    #endregion // PROTECTED_METHODS
+}
+
