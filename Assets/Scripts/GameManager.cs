40a684caa4be9c67a75ecb7e4fc7244af3d771c2
365462129@qq.com <365462129@qq.com>
2019-12-13 11:00:10 +0800

    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
